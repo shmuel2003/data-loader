b7a7f74acf6cd70812558d6559934825fb5c43e4
shmuelgross <shmuelgross3@gmail.com>
2025-08-20 11:50:19 +0300

    create file commands

---
+++ scripts/commands.bat
@@ -0,0 +1,9 @@
+@echo off
+REM ======= User-config ==========
+set DOCKER_USER=<YOUR_DOCKERHUB_USERNAME>
+set IMAGE_NAME=enemy-soldiers-api
+set TAG=latest
+set NAMESPACE=<YOUR_OPENSHIFT_PROJECT>
+REM ===============================
+
+REM 0) Optional:
